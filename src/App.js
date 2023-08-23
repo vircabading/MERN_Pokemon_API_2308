1016a0a17925935ca2e3a4bd5d559754bb7ba053
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-08-22 22:39:53 -0500

    add fetch pokemon view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import NavBarView from "./components/NavBarView";
+import FetchPokemonView from "./components/FetchPokemonView";
 
 ///////////////////////////////////////////////////
 //  MAIN APPLICATION
@@ -13,6 +14,8 @@ function App() {
       {/* ///// Main Content Area //////////// */}
       <div className='container mt-4 p-2 round bg-white' >
         <h1>Pokemon API</h1>
+        <br />
+        <FetchPokemonView />
       </div>
     </main>
 
---
+++ src/components/FetchPokemonView.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css';
+
+///////////////////////////////////////////////////
+//  FETCH POKEMON VIEW
+///////////////////////////////////////////////////
+
+const FetchPokemonView = () => {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col btn bg-nav-button round'>
+                    <h3 className='text-yellow'>Fetch Pokemon</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FetchPokemonView;
