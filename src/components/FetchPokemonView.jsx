e9dfb2569024d6bf95a39e52e0631105f374bfa0
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-08-22 22:59:51 -0500

    add handleClick function which activates when Fetch Pokemon button is pressed

--- src/components/FetchPokemonView.jsx
+++ src/components/FetchPokemonView.jsx
@@ -6,10 +6,17 @@ import '../App.css';
 ///////////////////////////////////////////////////
 
 const FetchPokemonView = () => {
+
+    const handleClick = () => {
+        let currentDate = new  Date();
+        console.log(`*** Pokemon Fetch: ${ currentDate.toLocaleTimeString() }`);
+    }
+
     return (
         <div className='container'>
             <div className='row'>
-                <div className='col btn bg-nav-button round'>
+                <div className='col btn bg-nav-button round'
+                    onClick={e => handleClick()}>
                     <h3 className='text-yellow'>Fetch Pokemon</h3>
                 </div>
             </div>
